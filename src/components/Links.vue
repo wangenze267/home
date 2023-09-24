6e5dfdd54c1c4d2f058469ec5ec56b65b2603bb6
Ned <15041567431@126.com>
2023-09-24 15:31:20 +0800

    :art: 增加站点描述信息

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,8 +43,8 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Github, Slideshare, Toolbox
-, NewspaperRegular } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Github, Toolbox
+, Fish, ChartBarRegular, Snowman } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -74,9 +74,10 @@ const siteIcon = {
   Fire,
   LaptopCode,
   Github,
-  Slideshare,
   Toolbox,
-  NewspaperRegular
+  Fish,
+  ChartBarRegular,
+  Snowman
 };
 
 // 链接跳转
