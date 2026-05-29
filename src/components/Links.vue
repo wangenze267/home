0e066e822df158adb7570eab92adf3f29ce216c7
wangenze267 <15041567431@126.com>
2026-05-29 12:43:38 +0800

    feat: add Prompt icon to siteIcon

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,7 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Github, Toolbox
-, Fish, ChartBarRegular, Snowman } from "@vicons/fa"; // 注意使用正确的类别
+, Fish, ChartBarRegular, Snowman, Prompt } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -77,7 +77,8 @@ const siteIcon = {
   Toolbox,
   Fish,
   ChartBarRegular,
-  Snowman
+  Snowman,
+  Prompt
 };
 
 // 链接跳转
